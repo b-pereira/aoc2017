9d811c857226593e92d49f26d9aa121940c7a488
Bruno Pereira <pereirabruno05@gmail.com>
2017-12-02 17:49:07 +0000

    Add day 2 challenge

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+
+def main():
+    f = open("input.txt")
+    num_l = [[int(num) for num in row] for row in [elem.split() for elem in f]]
+    checksum = sum([(max(row) - min(row)) for row in num_l])
+    print(checksum)
+
+
+if __name__ == "__main__":
+    main()
+
