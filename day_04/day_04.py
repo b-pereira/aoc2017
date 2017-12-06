7d3e2a509a11bf399c0c50b601fc22ea9ef016f1
_bpp <pereirabruno05@gmail.com>
2017-12-06 13:25:28 +0000

    Add part 1 of day 4 challenge

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python
+
+
+# part 1
+
+def has_repeated(t):
+    for i, elem1 in enumerate(t):
+        if elem1 in t[i+1:]:
+            return True
+    return False
+
+
+def count_valid_passphrase(phrases):
+    return sum([1 for words in phrases if not has_repeated(words)])
+
+
+def main():
+    f = open('input.txt')
+    count = count_valid_passphrase([line.strip().split() for line in f])
+    print(count)
+
+
+main()
