790803225f6b714787ea6e62fbeb0cbabba496b6
_bpp <pereirabruno05@gmail.com>
2017-12-06 15:15:01 +0000

    Add part 2 of day 4 challenge

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -2,7 +2,7 @@
 
 
 # part 1
-
+# efficient
 def has_repeated(t):
     for i, elem1 in enumerate(t):
         if elem1 in t[i+1:]:
@@ -10,14 +10,33 @@ def has_repeated(t):
     return False
 
 
-def count_valid_passphrase(phrases):
-    return sum([1 for words in phrases if not has_repeated(words)])
+# part 2
+# not efficient but one-liner ;)
+def has_anagrams(t):
+    return sum(
+        [(sorted(t[i]) == sorted(t[j]))
+         for i in range(0, len(t)) for j in range(i+1, len(t))])
+
+
+def count_valid_passphrase(phrases, f):
+    return sum([1 for words in phrases if not f(words)])
 
 
 def main():
     f = open('input.txt')
-    count = count_valid_passphrase([line.strip().split() for line in f])
-    print(count)
+    phrases = [line.strip().split() for line in f]
+    count_repeated = count_valid_passphrase(phrases, has_repeated)
+    print(count_repeated)
+    f.seek(0)
+    count_anagrams = count_valid_passphrase(phrases, has_anagrams)
+    print(count_anagrams)
 
+if __name__ == "__main__":
+    main()
 
-main()
+# l1 = [["abcde", "fghij"],
+#       ["abcde", "xyz", "ecdab"],
+#       ["a", "ab", "abc", "abd", "abf", "abj"],
+#       ["iiii", "oiii", "ooii", "oooi", "oooo"],
+#       ["oiii", "ioii", "iioi", "iiio"]]
+# print(count_valid_passphrase(l1, has_anagrams))
