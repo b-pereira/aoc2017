8350c4556c4adffef5d0d4bd0b9f405e95f36149
Bruno Pereira <pereirabruno05@gmail.com>
2017-12-12 19:16:33 +0000

    Add part 1 of day 7 challenge

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -1,1 +1,81 @@
 #!/usr/bin/env python
+
+
+# if tree node is a leaf
+def is_leaf(node):
+    return len(node) == 0
+
+
+# I'm using dictionaries with the key type str and value type list and creating
+# dictionary key: str, value: dict. Thence, a node of the tree that is visited
+# is a dictionary or a list otherwise. I'm aware that this isn't the most
+# elegant mechanism to mark visited nodes and serves only to verify is exists
+# in the tree, but wasn't visited. All visited nodes are deleted.
+def is_visited(node):
+    return not(type(node) is list)
+
+
+# This solution uses a list od list containing the paths of the graph. I'm
+# using an auxiliary list, because I can modify the original tree, composing it
+# with previous branchs, resulting the original tree with only one key: the
+# root.
+def build_tree(tree, ls):
+    d = dict()
+    for elem in ls:
+
+        if type(elem) is str and elem in tree:
+
+            if not is_visited(tree[elem]):
+
+                if is_leaf(tree[elem]):
+
+                    d[elem] = dict()
+                else:
+                    d[elem] = build_tree(tree, tree[elem])
+
+            else:
+                d[elem] = tree[elem]
+
+            # removing previous visited nodes
+            del tree[elem]
+
+        elif type(elem) is tuple:
+
+            key, value = elem
+
+            d[key] = build_tree(tree, value)
+
+            # if the return subtree has children
+            if len(d[key]) > 0:
+                tree[key] = d[key]
+
+            del d[key]
+
+    return d
+
+
+def main():
+
+    f = open('input.txt')
+
+    tree = dict()
+    path = []
+
+    for line in f:
+        program_info = line.replace('\n', '').split("->")
+        program = program_info[0].split()
+        name = program[0]
+        adjacency = []
+        if len(program_info) >= 2:
+            adjacency = program_info[1].replace(' ', '').split(',')
+
+        if len(adjacency) > 0:
+            path.append((name, adjacency))
+        tree[name] = adjacency
+
+    build_tree(tree, path)
+    print(list(tree.keys())[0])
+
+
+if __name__ == "__main__":
+    main()
