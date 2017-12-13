ef58d5765aea186189af0f65ecc9ce33f91e83b4
Bruno Pereira <pereirabruno05@gmail.com>
2017-12-13 23:25:12 +0000

    Remove redundant code

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -11,7 +11,7 @@ def has_repeated(t):
 
 
 # part 2
-# not efficient but one-liner ;)
+# not efficient but one-liner ;).
 def has_anagrams(t):
     return sum(
         [(sorted(t[i]) == sorted(t[j]))
@@ -24,19 +24,16 @@ def count_valid_passphrase(phrases, f):
 
 def main():
     f = open('input.txt')
+
     phrases = [line.strip().split() for line in f]
+
     count_repeated = count_valid_passphrase(phrases, has_repeated)
+
     print(count_repeated)
-    f.seek(0)
+
     count_anagrams = count_valid_passphrase(phrases, has_anagrams)
     print(count_anagrams)
 
 if __name__ == "__main__":
     main()
 
-# l1 = [["abcde", "fghij"],
-#       ["abcde", "xyz", "ecdab"],
-#       ["a", "ab", "abc", "abd", "abf", "abj"],
-#       ["iiii", "oiii", "ooii", "oooi", "oooo"],
-#       ["oiii", "ioii", "iioi", "iiio"]]
-# print(count_valid_passphrase(l1, has_anagrams))
