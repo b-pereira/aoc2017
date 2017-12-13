f6ab563cd3223798428a608a4e68a63cb2f020d6
Bruno Pereira <pereirabruno05@gmail.com>
2017-12-13 23:26:32 +0000

    Remove redundant code

--- day_05/day_05.py
+++ day_05/day_05.py
@@ -15,6 +15,7 @@ def trampoline(t):
     return count_steps
 
 
+# part 2
 def trampoline2(t):
     current_pos = 0
     jump = 0
@@ -37,21 +38,9 @@ def main():
 
     t = [int(line.strip()) for line in f]
 
-    # print(trampoline(t))
+    print(trampoline(t))
 
     print(trampoline2(t))
-    # l1 = [0, 3, 0, 1, -3]
-    # print(trampoline2(l1))
-
-    # l1 = [0, 3, 0, 1, -3]
-
-    # print('total', trampoline(l1))
-    # l2 = [1, 3, 0, 1, -3]
-    # l3 = [2, 3, 0, 1, -3]
-    # l4 = [2, 4, 0, 1, -3]
-    # l5 = [2, 4, 0, 1, -2]
-
-    # 372671
 
 
 if __name__ == "__main__":
