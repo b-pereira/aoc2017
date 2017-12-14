97b3b03e0ea3b4e3128c2fa93adc28b3f8cf978e
Bruno Pereira <pereirabruno05@gmail.com>
2017-12-14 22:48:09 +0000

    Add part 2 of day 7 challenge

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -55,17 +55,78 @@ def build_tree(tree, ls):
     return d
 
 
+def add_tree_weights(tree, table):
+
+    lst = []
+    if len(tree) == 0:
+        return lst
+
+    for key, value in tree.items():
+        lst.append(tuple([key, table[key], add_tree_weights(value, table)]))
+
+    return lst
+
+
+def most_and_least_freq(lst):
+    d = dict()
+
+    for elem in lst:
+        d[elem] = d.get(elem, 0) + 1
+    maximum = max(d.values())
+    minimum = min(d.values())
+
+    key_max = 0
+    key_min = 0
+    for key, value in d.items():
+        if value == maximum:
+            key_max = key
+        elif value == minimum:
+            key_min = key
+    return key_max, key_min
+
+
+def tree_balance(tree, table):
+
+    lst_weights = []
+    lst_keys = []
+    if len(tree) == 0:
+        return 0
+
+    for key, weight, elem in tree:
+
+        weight += tree_balance(elem, table)
+        lst_weights.append(weight)
+        lst_keys.append(key)
+
+    if (max(lst_weights) - min(lst_weights)) != 0:
+        most_freq, least_freq = most_and_least_freq(lst_weights)
+        unbalanced_index = lst_weights.index(least_freq)
+        unbalanced_node = lst_keys[unbalanced_index]
+        print('Name', unbalanced_node)
+        print('Wrong weight', table[unbalanced_node])
+        wrong_diff = abs(most_freq - least_freq)
+        print('Correct weight', table[unbalanced_node] - wrong_diff)
+        del lst_weights[unbalanced_index]
+        lst_weights.append(most_freq)
+
+    return sum(lst_weights)
+
+
 def main():
 
     f = open('input.txt')
 
     tree = dict()
+    tree_weights = dict()
+    table = dict()
     path = []
 
     for line in f:
         program_info = line.replace('\n', '').split("->")
         program = program_info[0].split()
+
         name = program[0]
+        weight = eval(program[1])
         adjacency = []
         if len(program_info) >= 2:
             adjacency = program_info[1].replace(' ', '').split(',')
@@ -73,9 +134,12 @@ def main():
         if len(adjacency) > 0:
             path.append((name, adjacency))
         tree[name] = adjacency
+        table[name] = weight
 
     build_tree(tree, path)
-    print(list(tree.keys())[0])
+
+    tree_weights = add_tree_weights(tree, table)
+    print('Total balanced weight', tree_balance(tree_weights, table))
 
 
 if __name__ == "__main__":
